d57a64ff915a9b7ac58efe880ddd08a945b3b565
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-28 15:22:47 -0500

    css to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,15 @@
+
+
 const Nav = () => {
     return (
-        <div>
-            <div>
+        <div className="top-nav">
+            <div className="nav-left">
                 <span>Write a Review</span>
                 <span>Events</span>
+            </div>
+            <div className="nav-right">
+                <span>Login</span>
+                <button className="button">Sign Up</button>
             </div>
         </div>
     )
