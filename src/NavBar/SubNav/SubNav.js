37d1e2c2af30ea512c2a718b0c48e6acae33b1e3
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-30 14:57:20 -0500

    finished sub nav bar

--- src/NavBar/SubNav/SubNav.js
+++ src/NavBar/SubNav/SubNav.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { SubNavItem } from './SubNavItem/SubNavItem'
+import styles from './SubNav.module.css'
 
 export function SubNav() {
   return (
-    <div>
-      <SubNavItem label="Restaurant" icon="fa-utensils" />
-      <SubNavItem label="Home Services" icon="fa-home" />
-      <SubNavItem label="Auto Services" icon="fa-car" />
-      <SubNavItem label="More" icon="fa-info-circle" showRightBorder />
+    <div className={styles.container}>
+      <div className={styles['sub-nav']}>
+        <div>
+          <SubNavItem label="Restaurant" icon="fa-utensils" />
+          <SubNavItem label="Home Services" icon="fa-home" />
+          <SubNavItem label="Auto Services" icon="fa-car" />
+          <SubNavItem label="More" icon="fa-info-circle" showRightBorder />
+        </div>
+        <div>
+          <button className={`button ${styles['subnav-button']}`}>
+            <span className="icon">
+              <i className="fas fa-pen"></i>
+            </span>
+            <span>Write a Review</span>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
