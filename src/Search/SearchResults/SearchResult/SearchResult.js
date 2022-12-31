6489771b79274b64c9c820a42b6adc41d74dacf1
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-31 17:28:12 -0500

    added image, ratings, and address to search result page

--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -4,9 +4,25 @@ import styles from './SearchResult.module.css'
 export function SearchResult() {
   return (
     <div className={styles['search-result']}>
-      <img src="http://via.placeholder.com/150" alt="business image" />
-      <p>General Info</p>
-      <p>Address Data</p>
+      <img
+        src="http://via.placeholder.com/150"
+        alt="business"
+        className={styles['business-image']}
+      />
+      <div className={styles['business-info']}>
+        <h2 className="subtitle">Burger Place</h2>
+        <p>Rating</p>
+        <p>
+          $$ <span className="tag">Burgers</span>{' '}
+          <span className="tag">Fast Food</span>
+        </p>
+      </div>
+      <div>
+        <p>+123483872</p>
+        <p>Example Street 5</p>
+        <p>12345</p>
+        <p>New York</p>
+      </div>
     </div>
   )
 }
