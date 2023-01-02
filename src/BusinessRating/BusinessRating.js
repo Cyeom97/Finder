8a0611a35e7e5a45ae0667cad833c87ae5b9ff7c
Christopher Yeom <chrisyeom97@gmail.com>
2023-01-02 14:39:14 -0500

    added names, image, and ratings

--- src/BusinessRating/BusinessRating.js
+++ src/BusinessRating/BusinessRating.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import styles from './BusinessRating.module.css'
-import Rating from 'react-rating'
 
-const BusinessRating = () => {
+const BusinessRating = (props) => {
   return (
     <div className={styles.rating}>
-      {/* <Rating
-      emptySymbol="far fa-star"
-      fullSymbol="fas fa-star"
-      fractions={2}
-      readonly
-      initialRating={3}
-      /> */}
-      <p>724 Reviews</p>
+      <p>
+        {props.rating} <i class="fa-solid fa-star"></i>
+      </p>
+      <p>{props.reviewCount} Reviews</p>
     </div>
   )
 }
--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -2,27 +2,30 @@ import React from 'react'
 import BusinessRating from '../../../BusinessRating/BusinessRating'
 import styles from './SearchResult.module.css'
 
-const SearchResult = () => {
+const SearchResult = (props) => {
+  if (!props.businesses) {
+    return <div></div>
+  }
   return (
     <div className={styles['search-result']}>
       <img
-        src="http://via.placeholder.com/150"
+        src={props.businesses.image_url}
         alt="business"
         className={styles['business-image']}
       />
       <div className={styles['business-info']}>
-        <h2 className="subtitle">Burger Place</h2>
-        <BusinessRating />
+        <h2 className="subtitle">{props.businesses.name}</h2>
+        <BusinessRating
+          reviewCount={props.businesses.review_count}
+          rating={props.businesses.rating}
+        />
         <p>
           $$ <span className="tag">Burgers</span>{' '}
           <span className="tag">Fast Food</span>
         </p>
       </div>
       <div className={styles['contact-info']}>
-        <p>+123483872</p>
-        <p>Example Street 5</p>
-        <p>12345</p>
-        <p>New York</p>
+        <p>{props.businesses.location.display_address}</p>
       </div>
     </div>
   )
